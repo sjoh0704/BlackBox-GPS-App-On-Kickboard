8d3fd03d70a4fb03bf100ce66c59737c6e6f22fe
amp3670 <amp3670@gmail.com>
2020-11-19 18:34:51 +0900

    .

--- app/src/main/java/org/techtown/blackbox/LoginActivity.java
+++ app/src/main/java/org/techtown/blackbox/LoginActivity.java
@@ -49,6 +49,8 @@ public class LoginActivity extends AppCompatActivity {
         btn_register = findViewById(R.id.btn_register);
         long now = System.currentTimeMillis();
 
+        getHashKey();
+
         // 회원가입 버튼을 클릭 시 수행
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,8 +87,9 @@ public class LoginActivity extends AppCompatActivity {
                                 String UserPPhone = jsonObject.getString("UserPPhone");
 
                                 Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
+                                //Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
 
+                                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
                                 // 아이디, parent_phone 데이터 전송
                                 intent.putExtra("UserPPhone", UserPPhone);
                                 intent.putExtra("UserId", UserId);
@@ -109,6 +112,27 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 
 }
